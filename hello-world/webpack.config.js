eed380573069b238211435b88029fe4cc33756a8
chenliunan <liunanchen520@gmail.com>
2024-03-29 14:48:56 +0800

    调整项目结构

--- hello-world/webpack.config.js
+++ hello-world/webpack.config.js
@@ -2,14 +2,36 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './index.js',
+  entry: {
+    cln: './index.js',
+  },
   output: {
-    filename: './main.js',
+    filename: 'main.js',
     path: path.join(__dirname, 'dist'),
+    // publicPath: './cln',
+    clean: true,
+  },
+  devServer: {
+    port: 3000,
+    static: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        // exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+    ],
   },
   mode: 'development',
   plugins: [new HtmlWebpackPlugin({
-    // template: './index.html',
+    template: './index.html',
     scriptLoading: 'blocking',
   })],
 }
