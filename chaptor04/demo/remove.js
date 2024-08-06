b25353496cee88a6fcaa3997ac3b007bc45cb6f6
chenliunan <liunanchen520@gmail.com>
2024-08-06 18:52:25 +0800

    chaptor04 custom loader

--- chaptor04/demo/remove.js
+++ chaptor04/demo/remove.js
@@ -1,9 +1,15 @@
-module.exports = function(content) {
+module.exports = function(content, map, meta) {
+  console.log('remove', map);
   if (typeof content === 'string') {
     const reg = /console.log\(.*\)/
-    console.log(content.match(reg))
+    // console.log(content.match(reg))
     content = content.replace(reg, "// remove console.log")
   }
 
+  // this.callback(
+  //   null,
+  //   content,
+  //   {}
+  // )
   return content
 }
--- chaptor04/demo/strict.js
+++ chaptor04/demo/strict.js
@@ -1,14 +1,39 @@
-const loaderUtils = require('loader-utils')
+const loaderUtils = require('loader-utils');
+const { SourceMapConsumer, SourceNode } = require('source-map');
+
+// console.log(loaderUtils);
 
 // return a Buffer or String
-module.exports = function(content) {
-  const options = this.getOptions()
-  console.log(options)
-  if (this.cacheable) {
-    this.cacheable();
-  }
-  
+module.exports = function(content, sourceMap) {
   var prefix = "\"use strict\";"
-  console.log(typeof content)
-  return prefix + content;
+  this.cacheable && this.cacheable();
+  const options = this.getOptions() || {};
+
+
+  
+
+  
+  if (!sourceMap) {
+    console.log('no');
+    return prefix + content;
+    // this.callback(
+    //   null,
+    //   prefix + content,
+    //   null,
+    // );
+    // return;
+  }
+  // console.log('yes');
+  // var currentRequest = loaderUtils.getCurrentRequest(this);
+  // console.log(this.remainingRequest);
+  // console.log('m>>', new SourceMapConsumer(sourceMap));
+  // var node = SourceNode.fromStringWithSourceMap(content, new SourceMapConsumer(sourceMap));
+  // node.prepend(prefix);
+  // var result = node.toStringWithSourceMap({ file: currentRequest });
+
+  // this.callback(
+  //   null,
+  //   result,
+  //   mappedSourceMap,
+  // )
 }
